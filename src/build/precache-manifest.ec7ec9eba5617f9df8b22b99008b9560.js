0a926a060b469be12b1ccff12f849398d37968d1
Marc Concepcion <MAConcep00@live.com>
2020-07-22 18:04:46 -0400

    Initial commit

---
+++ src/build/precache-manifest.ec7ec9eba5617f9df8b22b99008b9560.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ff4552b56e3ef1f724e0ceac51df77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6fd8fa6845a0dc68e4",
+    "url": "/static/css/main.93163d4a.chunk.css"
+  },
+  {
+    "revision": "cb0b1974081f8eb4f04a",
+    "url": "/static/js/2.63bee6c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.63bee6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6fd8fa6845a0dc68e4",
+    "url": "/static/js/main.5bc7e1a7.chunk.js"
+  },
+  {
+    "revision": "288b81f3f89e16098090",
+    "url": "/static/js/runtime-main.f266abb9.js"
+  }
+]);
